1fbb1e1e41d49f169753c6116e8409f936811e11
Debangshu <kanthadebangshu97@gmail.com>
2023-07-07 00:05:18 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react'
+
+// import './App.css'
+import { useEffect } from "react"
+import { fetchDataFromApi } from "./utils/api"
+import {useSelector, useDispatch } from 'react-redux';
+import { getApiConfiguration } from "./store/homeSlice";
+function App() {
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    apiTesting();
+  },[]);
+  
+const apiTesting =()=>{
+  fetchDataFromApi('/movie/popular').then(res => {console.log(res);
+  dispatch(getApiConfiguration)})
+}
+  return (
+    <div className='App'>
+     App 
+    </div>
+  )
+}
+
+export default App
