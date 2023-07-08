1944e60040bc70753d1c11cadef772e14689b355
Debangshu <kanthadebangshu97@gmail.com>
2023-07-08 23:45:09 +0530

    pages start

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 // import { useState } from 'react'
 
+import {BrowserRouter,Route,Routes} from "react-router-dom";
+import PageNotFound from "./pages/404/pageNotFound";
+import Explore from "./pages/explore/Explore";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Details from "./pages/details/Details";
+import Home from "./pages/home/Home";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
 // import './App.css'
 import { useEffect } from "react"
 import { fetchDataFromApi } from "./utils/api"
@@ -7,18 +15,32 @@ import {useSelector, useDispatch } from 'react-redux';
 import { getApiConfiguration } from "./store/homeSlice";
 function App() {
   const dispatch = useDispatch()
+  const{url}= useSelector((state)=>state.home)
   useEffect(()=>{
-    apiTesting();
+    fetchApiConfig();
   },[]);
   
-const apiTesting =()=>{
-  fetchDataFromApi('/movie/popular').then(res => {console.log(res);
-  dispatch(getApiConfiguration)})
+const fetchApiConfig =()=>{
+  fetchDataFromApi('/configuration').then(res => {console.log(res);
+    const url={
+      backdrop:res.images.secure_base_url+"original",
+      poster:res.images.secure_base_url+"original",
+      profile:res.images.secure_base_url+"original",
+    }
+  dispatch(getApiConfiguration(url))})
 }
   return (
-    <div className='App'>
-     App 
-    </div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/:mediaType/:id" element={<Details />} />
+                <Route path="/search/:query" element={<SearchResult />} />
+                <Route path="/explore/:mediaType" element={<Explore />} />
+                <Route path="*" element={<PageNotFound />} />
+    </Routes>
+    <Footer></Footer>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/404/pageNotFound.jsx
@@ -0,0 +1,12 @@
+import "./style.scss";
+// import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PageNotFound
